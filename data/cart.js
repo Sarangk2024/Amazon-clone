65e6f83eccc0b4ca8866dac930f9207660256619
Sarang K <kuttappisarang629@gmail.com>
2025-12-18 19:45:48 +0530

    checkout completed

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,27 @@
-export let cart=[
+export let cart=JSON.parse(localStorage.getItem('cart'))
+
+
+if(!cart){
+    cart=[
     {
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity:2
+        quantity:2,
+        deliveryOptionId:'1'
     },
     {
         productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity:1
-    },
-    {
-        productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-        quantity:10
+        quantity:1,
+        deliveryOptionId:'1'
     }
 ];
+}
+
+
+
+
+function saveToStrorage(){
+    localStorage.setItem('cart',JSON.stringify(cart))
+}
 
 export function addToCart(productId){
   
@@ -28,9 +38,12 @@ export function addToCart(productId){
         {
             cart.push({
             productId:productId,
-            quantity:1
+            quantity:1,
+            deliveryOptionId:'1'
         });
         }
+
+        saveToStrorage();
 }
 
 export function removeFromCart(productId){
@@ -42,4 +55,20 @@ export function removeFromCart(productId){
     });
 
     cart=newCart;
+
+    saveToStrorage();
+}
+
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+    let matchingItem;
+
+        cart.forEach((cartItem)=>{
+            if(productId===cartItem.productId)
+                matchingItem=cartItem;
+        });
+
+    matchingItem.deliveryOptionId=deliveryOptionId
+
+    saveToStrorage()
 }
